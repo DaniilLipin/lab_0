1e20b00eb0714943d54d438269d1305d1556602e
DaniilLipin <70561371+DaniilLipin@users.noreply.github.com>
2020-09-22 10:24:06 +0500

    Add files via upload

---
+++ lab_0/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите букву");
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
